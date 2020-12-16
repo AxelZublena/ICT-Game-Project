505f1f4a5d6fb63ed6ac2ecc2b759b0ebdc46d44
AxelZublena <zubl0001@hz.nl>
2020-12-16 13:25:09 +0100

    Added start menu + view

--- src/Game.ts
+++ src/Game.ts
@@ -1,10 +1,73 @@
 class Game {
-    private canvas: HTMLCanvasElement;
+	private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
 
-    constructor(canvas: HTMLElement) {
-        this.canvas = <HTMLCanvasElement>canvas;
-        this.canvas.width = 1000;
-        this.canvas.height = 1000;
+	private startMenu: StartMenu;
+    private randomView: View;
+    private currentView: View;
+
+    public static readonly BASE_COLOR: string = "#00A5DC";
+
+	constructor(canvas: HTMLElement) {
+		this.canvas = <HTMLCanvasElement>canvas;
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+        this.ctx = this.canvas.getContext("2d");
+
+        // create a start menu
+		this.startMenu = new StartMenu(this.canvas, Game.BASE_COLOR);
+        // event listener to handle mouse actions;
+        this.mouseHandling();
+
+        // randomView to test the start menu
+        this.randomView = new Pause(this.canvas);
+
+        this.currentView = this.startMenu;
+
+		requestAnimationFrame(this.step);
+	}
+
+	/**
+	 * This MUST be an arrow method in order to keep the `this` variable
+	 * working correctly. It will be overwritten by another object otherwise
+	 * caused by javascript scoping behaviour.
+	 */
+	step = () => {
+        // draw the current view
+        this.draw();
+
+		// Call this method again on the next animation frame
+		// The user must hit F5 to reload the game
+		requestAnimationFrame(this.step);
+	};
+
+    // Draw on the canvas
+    private draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Draw the current view
+		this.currentView.draw(this.ctx);
     }
 
+    // Handle the mouse's events (only works for the start menu, but can and will be updated)
+    private mouseHandling(){
+		this.canvas.addEventListener("mousemove", (event) => {
+			if (this.startMenu.isOnButton(event, this.startMenu.getButton())) {
+                this.startMenu.setButtonColor("grey");
+				document.getElementById("canvas").style.cursor = "pointer";
+			} else {
+                this.startMenu.setButtonColor(Game.BASE_COLOR);
+				document.getElementById("canvas").style.cursor = "default";
+			}
+		});
+		this.canvas.addEventListener("mousedown", (event) => {
+			if (this.startMenu.isOnButton(event, this.startMenu.getButton())) {
+                this.currentView = this.randomView;
+				console.log("Start the game!");
+			} else {
+				console.log("Don't start the game.");
+			}
+		});
+    }
 }
+
---
+++ src/StartMenu.ts
@@ -0,0 +1,89 @@
+/// <reference path="View.ts" />
+
+class StartMenu extends View {
+	private startButton: any;
+	private buttonColor: string;
+	private image: HTMLImageElement;
+
+	public constructor(canvas: HTMLCanvasElement, buttonColor: string) {
+		super(canvas);
+
+		// empty startButton
+		this.startButton = {
+			width: 0,
+			height: 0,
+			x: 0,
+			y: 0,
+		};
+
+		// set the color of the button
+		this.buttonColor = buttonColor;
+
+		// load the image/logo
+		this.image = this.loadNewImage("/assets/img/hacker.png");
+	}
+
+	/**
+	 * Method to set the button's color
+	 * @param {string} color
+	 */
+	public setButtonColor(color: string) {
+		this.buttonColor = color;
+	}
+
+	/**
+	 * Method to get the button's information
+	 */
+	public getButton() {
+		return this.startButton;
+	}
+
+	/**
+	 * Method to draw the start menu on the canvas
+	 * @param {CanvasRenderingContext2D} ctx
+	 */
+	public draw(ctx: CanvasRenderingContext2D) {
+		// Draw the background color
+		ctx.fillStyle = Game.BASE_COLOR;
+		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+		// Gets the button's information in an object for isOnButton()
+		this.startButton.width = this.canvas.width * 0.25;
+		this.startButton.height = this.canvas.height * 0.19;
+		this.startButton.x = this.canvas.width / 2;
+		this.startButton.y = this.canvas.height * 0.77;
+
+		// Draws the image/logo
+		ctx.drawImage(
+			this.image,
+			this.canvas.width / 2 - ((this.canvas.height * 0.9) / 2) * 0.97,
+			this.canvas.height / 2 - (this.canvas.height * 0.9) / 2,
+			this.canvas.height * 0.9,
+			this.canvas.height * 0.9
+		);
+
+		// Draws a round rectangle
+		this.roundRect(
+			ctx,
+			this.startButton.x,
+			this.startButton.y,
+			this.startButton.width,
+			this.startButton.height,
+			20,
+			this.buttonColor,
+            8,
+            "white"
+		);
+
+		// Write text in the button
+		this.writeTextToCanvas(
+			ctx,
+			"START",
+			this.canvas.width * 0.05,
+			this.canvas.width * 0.5,
+			this.canvas.height * 0.8,
+			"center",
+			"white"
+		);
+	}
+}
