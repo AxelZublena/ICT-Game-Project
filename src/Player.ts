8400380f599fc48bbf3d875a9e9a924429cf64c9
Hao Guo <guo0012@hz.nl>
2020-12-16 13:56:22 +0100

    added player class

---
+++ src/Player.ts
@@ -0,0 +1,15 @@
+class Player {
+
+    private name: string;
+    private speed: number;
+    private xPos: number;
+    private yPos: number;
+    private image: string;
+
+    public constructor(name: string){
+        this.image = "image";
+        this.name = name;
+        this.speed = 5;
+    }
+
+}
